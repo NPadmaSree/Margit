43307528cb54b03db943136b59e79a10905afadc
student <student@BC-minimac021s-Mac-mini.local>
2022-08-23 14:44:37 +0300

    added calc

---
+++ calculators/script.js
@@ -0,0 +1,8 @@
+function calculate() {
+  let price = +getElementById("price");
+  let money = +getElementById("money");
+
+  console.log(
+    `" could get about"${money / price} "liters\, good\, now ypu can escape"`
+  );
+}
