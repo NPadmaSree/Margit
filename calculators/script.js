d199fc71d91d7d187ea5347dea543e15a9f3b695
Nandini Padma Sree <s2201210@BC-C02J61D7Q6LR-MacBook-Air.local>
2022-08-24 12:42:41 +0300

    some message

--- calculators/script.js
+++ calculators/script.js
@@ -1,8 +1,31 @@
-function calculate() {
-  let price = +getElementById("price");
-  let money = +getElementById("money");
+// function calculate() {
+//   let price = +getElementById("price");
+//   let money = +getElementById("money");
+//   if(money/price>10){
+//     console.log(
+//       `" could get about"${money / price} "liters\, good\, now ypu can escape"`
+//     ); 
 
-  console.log(
-    `" could get about"${money / price} "liters\, good\, now ypu can escape"`
-  );
+//   } else {
+
+//   }
+
+//   console.log()
+// }
+const calculate = () => {
+  const price=Number(document.getElementById("price").value);
+  const money=+document.querySelector("#money").value;
+  const answer=document.querySelector('#answer');
+  //console.log('price', price,'money', money);
+  const amount=Math.floor(money/price);
+  //console.log('amount', amount);
+  let text;
+  if(amount>10) {
+    text=`You could get about ${amount} liters, good, now you van escape`;
+
+} else{
+  text=`You could get about ${amount} liters, sorry, you have to stay`;
 }
+  answer.textContent=amount;
+
+};
